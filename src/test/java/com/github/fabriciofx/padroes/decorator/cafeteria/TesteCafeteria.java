bdd362c4fbe7c434d635fe1d93f887c58c821499
Fabricio Cabral <fabriciofx@gmail.com>
2016-01-13 23:40:41 -0300

    Refatoração do teste da cafeteira para utilizar junit.

--- src/test/java/com/github/fabriciofx/padroes/decorator/cafeteria/TesteCafeteria.java
+++ src/test/java/com/github/fabriciofx/padroes/decorator/cafeteria/TesteCafeteria.java
@@ -1,16 +1,27 @@
 package com.github.fabriciofx.padroes.decorator.cafeteria;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
 import com.github.fabriciofx.apoo.padroes.decorator.cafeteria.Canela;
 import com.github.fabriciofx.apoo.padroes.decorator.cafeteria.Chantilly;
 import com.github.fabriciofx.apoo.padroes.decorator.cafeteria.Chocolate;
 import com.github.fabriciofx.apoo.padroes.decorator.cafeteria.Expresso;
-import com.github.fabriciofx.apoo.padroes.decorator.cafeteria.Produto;
 import com.github.fabriciofx.apoo.padroes.decorator.cafeteria.Leite;
+import com.github.fabriciofx.apoo.padroes.decorator.cafeteria.Produto;
 
 public final class TesteCafeteria {
-	public static void main(String[] args) {
+
+	@Test
+	public void criaUmCappuccino() {
 		final Produto cappuccino = new Chantilly(
 				new Canela(new Chocolate(new Leite(new Expresso()))));
-		System.out.println("Preço do Cappuccino: " + cappuccino.preco());
+		
+		assertEquals(3.8, cappuccino.preco(), 0.0001);
+		assertEquals("[Expresso, Leite, Chocolate, Canela, Chantilly]",
+				Arrays.toString(cappuccino.composicao().toArray()));
 	}
 }
