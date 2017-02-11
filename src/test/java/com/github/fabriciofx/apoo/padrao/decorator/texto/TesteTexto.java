ec7fa64931afc11afcf033cce828a78cb21df5aa
Fabricio Cabral <fabriciofx@gmail.com>
2017-02-11 06:24:20 -0300

    Organização do código do teste de Texto

--- src/test/java/com/github/fabriciofx/apoo/padrao/decorator/texto/TesteTexto.java
+++ src/test/java/com/github/fabriciofx/apoo/padrao/decorator/texto/TesteTexto.java
@@ -14,73 +14,98 @@ import com.github.fabriciofx.apoo.padrao.decorator.texto.TextoFiltrado;
 import com.github.fabriciofx.apoo.padrao.decorator.texto.TextoMaiusculo;
 
 public final class TesteTexto {
-	private static final String LS = System.lineSeparator();
-	private static final String TEXTO =
-			"  fabrício   barros      cabral    " + LS
-			+ "rua Do AsQUER 1982  	capital     " + LS
-			+ "Pedro		do     BÓ";	
-	private static final String ARQUIVO = TesteTexto.class
-			.getResource("/texto.txt").getFile();
-
 	@Test
 	public void aparadoEMaiusculo() throws IOException {
-		final Texto texto = new TextoMaiusculo(
-				new TextoLimpo(
-						new TextoSimples(TEXTO)
-				)
+		final String eol = System.lineSeparator();
+		final String conteudo = String.format(
+			"%s%s%s%s%s",
+			"  fabrício   barros      cabral    ",
+			eol,
+			"rua Do AsQUER 1982  	capital     ",
+			eol,
+			"Pedro		do     BÓ"	
 		);
-		
-		assertEquals(texto.conteudo(),
-				"FABRÍCIO BARROS CABRAL" + LS
-				+ "RUA DO ASQUER 1982 CAPITAL" + LS
-				+ "PEDRO DO BÓ"
+		final Texto texto = new TextoMaiusculo(
+			new TextoLimpo(
+				new TextoSimples(conteudo)
+			)
+		);		
+		assertEquals(
+			texto.conteudo(),
+			String.format(
+				"%s%s%s%s%s",
+				"FABRÍCIO BARROS CABRAL",
+				eol,
+				"RUA DO ASQUER 1982 CAPITAL",
+				eol,
+				"PEDRO DO BÓ"
+			)
 		);
 	}
 
 	@Test
 	public void aparadoMaiusculoEFiltrado() throws IOException {
-		final Texto texto = new TextoFiltrado(
-				new TextoMaiusculo(
-						new TextoLimpo(
-								new TextoSimples(TEXTO)
-						)
-				),
-				".*PEDRO.*"
+		final String eol = System.lineSeparator();
+		final String conteudo = String.format(
+			"%s%s%s%s%s",
+			"  fabrício   barros      cabral    ",
+			eol,
+			"rua Do AsQUER 1982  	capital     ",
+			eol,
+			"Pedro		do     BÓ"	
 		);
-
+		final Texto texto = new TextoFiltrado(
+			new TextoMaiusculo(
+				new TextoLimpo(
+					new TextoSimples(conteudo)
+				)
+			),
+			".*PEDRO.*"
+		);
 		assertEquals(texto.conteudo(), "PEDRO DO BÓ");
 	}
 	
 	@Test
 	public void aparadoEMaiusculoDoArquivo() throws IOException {
+		final String eol = System.lineSeparator();
+		final String arquivo = TesteTexto.class
+			.getResource("/texto.txt")
+			.getFile();
 		final Texto texto = new TextoMaiusculo(
-				new TextoLimpo(
-						new TextoArquivado(
-								new File(ARQUIVO)
-						)
-				)
+			new TextoLimpo(
+					new TextoArquivado(
+							new File(arquivo)
+					)
+			)
 		);
-		
-		assertEquals(texto.conteudo(),
-				"FABRÍCIO BARROS CABRAL" + LS
-				+ "RUA DO ASQUER 1982 CAPITAL" + LS
-				+ "PEDRO DO BÓ"
+		assertEquals(
+			texto.conteudo(),
+			String.format(
+				"%s%s%s%s%s",
+				"FABRÍCIO BARROS CABRAL",
+				eol,
+				"RUA DO ASQUER 1982 CAPITAL",
+				eol,
+				"PEDRO DO BÓ"
+			)
 		);
 	}
 
 	@Test
 	public void aparadoMaiusculoEFiltradoDoArquivo() throws IOException {
+		final String arquivo = TesteTexto.class
+			.getResource("/texto.txt")
+			.getFile();
 		final Texto texto = new TextoFiltrado(
-				new TextoMaiusculo(
-						new TextoLimpo(
-								new TextoArquivado(
-										new File(ARQUIVO)
-								)
-						)
-				),
-				".*PEDRO.*"
+			new TextoMaiusculo(
+				new TextoLimpo(
+					new TextoArquivado(
+						new File(arquivo)
+					)
+				)
+			),
+			".*PEDRO.*"
 		);
-
 		assertEquals(texto.conteudo(), "PEDRO DO BÓ");
 	}
 }
